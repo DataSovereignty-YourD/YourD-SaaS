e1c061300932387055644e49153aaa2aaa44dd63
parkerynch <110054754+parkerynch@users.noreply.github.com>
2023-08-23 03:09:39 +0900

    projectTopBar2

--- src/components/project/sideBar.tsx
+++ src/components/project/sideBar.tsx
@@ -1,9 +1,9 @@
-import { Link, useLocation } from 'react-router-dom';
-import { useState } from 'react';
-import { projectType } from '../../recoil/dashBoard/project';
-import { useRecoilState } from 'recoil';
+import { Link, useLocation } from "react-router-dom";
+import { useState } from "react";
+import { projectType } from "../../recoil/dashBoard/project";
+import { useRecoilState } from "recoil";
 
-import { sideBarToggleState } from '../../recoil/sideBarToggle';
+import { sideBarToggleState } from "../../recoil/sideBarToggle";
 
 export default function SideBar({ item }: { item: projectType }) {
   const location = useLocation();
@@ -15,46 +15,43 @@ export default function SideBar({ item }: { item: projectType }) {
 
   let sideBarData = [
     {
-      name: 'Dashboard',
-      path: 'dashboard',
-      description: 'Data overview',
+      name: "Dashboard",
+      path: "dashboard",
+      description: "Data overview",
       svgPath:
-        'M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25',
+        "M2.25 12l8.954-8.955c.44-.439 1.152-.439 1.591 0L21.75 12M4.5 9.75v10.125c0 .621.504 1.125 1.125 1.125H9.75v-4.875c0-.621.504-1.125 1.125-1.125h2.25c.621 0 1.125.504 1.125 1.125V21h4.125c.621 0 1.125-.504 1.125-1.125V9.75M8.25 21h8.25",
     },
     {
-      name: 'API KEY',
-      path: 'apikey',
-      description: 'Manage API KEY',
+      name: "API KEY",
+      path: "apikey",
+      description: "Manage API KEY",
       svgPath:
-        'M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z',
+        "M19.5 14.25v-2.625a3.375 3.375 0 00-3.375-3.375h-1.5A1.125 1.125 0 0113.5 7.125v-1.5a3.375 3.375 0 00-3.375-3.375H8.25m2.25 0H5.625c-.621 0-1.125.504-1.125 1.125v17.25c0 .621.504 1.125 1.125 1.125h12.75c.621 0 1.125-.504 1.125-1.125V11.25a9 9 0 00-9-9z",
     },
     {
-      name: 'Users',
-      path: 'usermanagement',
-      description: 'Manage users',
+      name: "Users",
+      path: "usermanagement",
+      description: "Manage users",
       svgPath:
-        'M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z',
+        "M15 19.128a9.38 9.38 0 002.625.372 9.337 9.337 0 004.121-.952 4.125 4.125 0 00-7.533-2.493M15 19.128v-.003c0-1.113-.285-2.16-.786-3.07M15 19.128v.106A12.318 12.318 0 018.624 21c-2.331 0-4.512-.645-6.374-1.766l-.001-.109a6.375 6.375 0 0111.964-3.07M12 6.375a3.375 3.375 0 11-6.75 0 3.375 3.375 0 016.75 0zm8.25 2.25a2.625 2.625 0 11-5.25 0 2.625 2.625 0 015.25 0z",
     },
 
     {
-      name: 'Settings',
-      path: 'settings',
-      description: 'Edit settings',
+      name: "Settings",
+      path: "settings",
+      description: "Edit settings",
       svgPath:
-        'M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z',
+        "M9.594 3.94c.09-.542.56-.94 1.11-.94h2.593c.55 0 1.02.398 1.11.94l.213 1.281c.063.374.313.686.645.87.074.04.147.083.22.127.324.196.72.257 1.075.124l1.217-.456a1.125 1.125 0 011.37.49l1.296 2.247a1.125 1.125 0 01-.26 1.431l-1.003.827c-.293.24-.438.613-.431.992a6.759 6.759 0 010 .255c-.007.378.138.75.43.99l1.005.828c.424.35.534.954.26 1.43l-1.298 2.247a1.125 1.125 0 01-1.369.491l-1.217-.456c-.355-.133-.75-.072-1.076.124a6.57 6.57 0 01-.22.128c-.331.183-.581.495-.644.869l-.213 1.28c-.09.543-.56.941-1.11.941h-2.594c-.55 0-1.02-.398-1.11-.94l-.213-1.281c-.062-.374-.312-.686-.644-.87a6.52 6.52 0 01-.22-.127c-.325-.196-.72-.257-1.076-.124l-1.217.456a1.125 1.125 0 01-1.369-.49l-1.297-2.247a1.125 1.125 0 01.26-1.431l1.004-.827c.292-.24.437-.613.43-.992a6.932 6.932 0 010-.255c.007-.378-.138-.75-.43-.99l-1.004-.828a1.125 1.125 0 01-.26-1.43l1.297-2.247a1.125 1.125 0 011.37-.491l1.216.456c.356.133.751.072 1.076-.124.072-.044.146-.087.22-.128.332-.183.582-.495.644-.869l.214-1.281z",
     },
   ];
 
   return (
     <div
       className={`min-h-[90vh] h-full ${
-        isSidebarVisible ? 'w-108' : 'w-15'
+        isSidebarVisible ? "w-108" : "w-15"
       } transform transition-transform duration-300 ease-in-out`}
     >
-      <button onClick={() => setSidebarVisible((prev) => !prev)}>Button</button>
-      <div
-        className={`min-h-[90vh] h-full `}
-      >
+      <div className={`min-h-[90vh] h-full `}>
         {/* {isSidebarVisible && (
           <>
             <p className="text-black text-mb font-medium mb-2">Welcome back,</p>
@@ -84,11 +81,11 @@ export default function SideBar({ item }: { item: projectType }) {
               onClick={() => setSelectedMenu(data.path)}
               className={`flex flex-col space-y-2 md:flex-row md:space-y-0 md:space-x-2 items-center group transition duration-150 ease-linear rounded-lg group w-full py-3 px-2 ${
                 isSidebarVisible && selectedMenu === data.path
-                  ? 'bg-gray-200'
-                  : 'hover:bg-white'
+                  ? "bg-gray-200"
+                  : "hover:bg-white"
               }`}
             >
-              <div className={isSidebarVisible ? 'w-6 h-6' : 'w-8 h-8'}>
+              <div className={isSidebarVisible ? "w-6 h-6" : "w-8 h-8"}>
                 <svg
                   xmlns="http://www.w3.org/2000/svg"
                   fill="none"
@@ -96,8 +93,8 @@ export default function SideBar({ item }: { item: projectType }) {
                   stroke="currentColor"
                   className={
                     selectedMenu === data.path
-                      ? 'text-indigo-500'
-                      : 'group-hover:text-indigo-400'
+                      ? "text-indigo-500"
+                      : "group-hover:text-indigo-400"
                   }
                 >
                   <path
--- src/components/projectTobBar.tsx
+++ src/components/projectTobBar.tsx
@@ -4,11 +4,13 @@ import { useRecoilState } from "recoil";
 import YourdLogo from "../assets/img/YourDLogo.png";
 import { projectModalState } from "../recoil/dashBoard/project";
 import React, { useState } from "react";
+import { sideBarToggleState } from "../recoil/sideBarToggle";
 
 export default function ProjectTopBar() {
   const navigation = useNavigate();
   const [isOpen, setIsOpen] = useRecoilState(projectModalState);
-
+  const [isSidebarVisible, setSidebarVisible] =
+    useRecoilState(sideBarToggleState);
   const [isSearchBarVisible, setSearchBarVisible] = useState(false);
 
   const handleSearchIconClick = () => {
@@ -16,11 +18,11 @@ export default function ProjectTopBar() {
   };
 
   return (
-    <div className="h-[48px] flex ">
+    <div className="h-[48px] flex items-center mx-10  ">
       <img
         src={YourdLogo}
         onClick={() => navigation("/")}
-        className="flex h-[48px] ml-16"
+        className="flex h-[48px] "
       />
       {/* <Button
                 className="py-2 bg-gray-500 rounded-[36px]  mt-3"
@@ -30,7 +32,8 @@ export default function ProjectTopBar() {
               </Button> */}
 
       <button
-        className="flex items-center justify-center p-2 text-gray-700 hover:bg-gray-300 rounded-md ml-9"
+        onClick={() => setSidebarVisible((prev) => !prev)}
+        className="flex items-center justify-center p-2 text-gray-700 hover:bg-gray-300 rounded-md "
         type="button"
       >
         <span>
@@ -49,11 +52,11 @@ export default function ProjectTopBar() {
       </button>
 
       <div className="flex justify-between w-full  ">
-        <div className="flex">
+        <div className="flex  items-center ">
           <div className="search-icon" onClick={handleSearchIconClick}>
             {
               <svg
-                className="h-8 w-8 text-blue-500 mt-2 ml-2"
+                className="h-8 w-8 text-blue-500 "
                 width="24"
                 height="24"
                 viewBox="0 0 24 24"
@@ -77,57 +80,55 @@ export default function ProjectTopBar() {
                 id="header-search"
                 placeholder="검색어를 입력하세요"
                 type="text"
-                className="w-48 py-1 px-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500 mt-2 ml-2"
+                className="w-48 px-2 rounded-md border border-gray-300 focus:outline-none focus:border-blue-500  "
                 value=""
               ></input>
             </div>
           )}
         </div>
 
-        <div className="">
-          <div className="flex">
-            <svg
-              className="h-8 w-8 text-gray-500 mt-2"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
-              />
-            </svg>
-
-            <svg
-              className="h-8 w-8 text-gray-600 mt-2 ml-2"
-              width="24"
-              height="24"
-              viewBox="0 0 24 24"
-              stroke-width="2"
-              stroke="currentColor"
-              fill="none"
+        <div className="flex items-center gap-4">
+          <svg
+            className="h-8 w-8 text-gray-500 "
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
               stroke-linecap="round"
               stroke-linejoin="round"
-            >
-              {" "}
-              <path stroke="none" d="M0 0h24v24H0z" />{" "}
-              <path d="M16.2 4a9.03 9.03 0 1 0 3.9 12a6.5 6.5 0 1 1 -3.9 -12" />
-            </svg>
+              stroke-width="2"
+              d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
+            />
+          </svg>
 
-            <img
-              className=" w-10 h-10 rounded-full object-cover mt-1 flex justify-end ml-3 mr-4"
-              src="http://wiki.hash.kr/images/d/d6/이더리움_로고.png"
-              alt="User Avatar"
-            ></img>
+          <svg
+            className="h-8 w-8 text-gray-600 "
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            stroke-width="2"
+            stroke="currentColor"
+            fill="none"
+            stroke-linecap="round"
+            stroke-linejoin="round"
+          >
+            {" "}
+            <path stroke="none" d="M0 0h24v24H0z" />{" "}
+            <path d="M16.2 4a9.03 9.03 0 1 0 3.9 12a6.5 6.5 0 1 1 -3.9 -12" />
+          </svg>
 
-            <img
-              className=" w-10 h-10 rounded-full object-cover mt-1 flex justify-end ml-1 mr-4"
-              src="https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=400"
-              alt="User Avatar"
-            ></img>
-          </div>
+          <img
+            className=" w-10 h-10 rounded-full object-cover  border border-red-700 "
+            src="http://wiki.hash.kr/images/d/d6/이더리움_로고.png"
+            alt="User Avatar"
+          ></img>
+
+          <img
+            className=" w-10 h-10 rounded-full object-cover   "
+            src="https://images.unsplash.com/photo-1567306226416-28f0efdc88ce?crop=entropy&amp;cs=tinysrgb&amp;fit=max&amp;fm=jpg&amp;ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw&amp;ixlib=rb-1.2.1&amp;q=80&amp;w=400"
+            alt="User Avatar"
+          ></img>
         </div>
       </div>
     </div>
