4f6435ad10bb0de875c744f9dc540124054aa86e
yeomjaeseung <struckpang@gmail.com>
2023-09-01 00:22:07 +0900

    apiKey image

--- src/components/newProjectModal.tsx
+++ src/components/newProjectModal.tsx
@@ -3,7 +3,7 @@ import { Button } from "react-bootstrap";
 import { useRecoilState } from "recoil";
 import { Close } from "../assets/icons/close";
 import { projectModalState, projectState } from "../recoil/dashBoard/project";
-import Dropdown from "./project/newProjectDropdown";
+import Dropdown from "./project/dropDown";
 import { AiOutlineClose } from "react-icons/ai";
 import useSessionStorage from "../function/sesstionStorage";
 export default function NewProjectModal({
